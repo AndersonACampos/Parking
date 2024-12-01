2cb47e7adf2a425a3566b461b16d4cc5bdecff5e
Anderson A Campos <cadeveloper@yahoo.com.br>
2024-12-01 19:52:01 -0300

    Tela de cadastro

--- View/Parking.pyw
+++ View/Parking.pyw
@@ -4,7 +4,7 @@ import PySimpleGUI as sg
 # Adiciona o diretório pai ao caminho de importação
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import BLL.TicketBLL as tb
-import time
+import Pagamento
 import Ticket
 
 sg.theme('TanBlue')
@@ -29,9 +29,7 @@ layout = [
               key='grdTickets'
               )],
     
-    [sg.Button("Fechar", 
-               key='cmdFechar'
-               )]
+    [sg.Button("Fechar", key='cmdFechar'), sg.Button("Cadastrar", key='cmdCadastrar')]
 ]
 
 window = sg.Window("Park v1.0", layout)
@@ -44,10 +42,13 @@ while True:
     if event == 'grdTickets':
         try:
             linha = values['grdTickets'][0]
-            Ticket.fechar_conta(ticket[linha])
+            Pagamento.fechar_conta(ticket[linha])
         except:
             pass
         ticket, colunas = carrega_ticket()
-        window['grdTickets'].update(values=ticket)   
+        window['grdTickets'].update(values=ticket)
+        
+    if event == 'cmdCadastrar':
+        Ticket.cadastrar()
              
 window.close()
--- View/Ticket.py
+++ View/Ticket.py
@@ -1,58 +1,28 @@
-import os
-import sys
 import PySimpleGUI as sg
-from datetime import datetime
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-import BLL.TicketBLL as tb
 
-def fechar_conta(ticket):
-    
-    codigo = str(ticket[0]).zfill(6)
-    entrada = datetime.strptime( ticket[1],'%Y-%m-%d %H:%M:%S').strftime('%d/%m/%Y %H:%M:%S')
-    agora = datetime.now().replace(microsecond=0)
-    saida = datetime.strptime( ticket[2],'%Y-%m-%d %H:%M:%S').strftime('%d/%m/%Y %H:%M:%S') if ticket[2] != '' else agora.strftime('%d/%m/%Y %H:%M:%S') 
-    tempo = tb.calcula_tempo(entrada, saida)
-    valor = tb.calcular_valor_pagar(tempo)
-    pagar = f"R$ {valor:,.2f}"
-    pago = ticket[8]
-    
-    if pago == 'Sim':
-        sg.popup(f'O ticket {codigo} já foi pago.')
-    
-    coluna_esqueda = [
-        [sg.Text(f'Ticket:')],
-        [sg.Text(f'Hora da Entrada:')],
-        [sg.Text(f'Hora da Saída:')],
-        [sg.Text(f'Tempo total em minutos:')],
-        [sg.Text(f'Total a pagar:')],
-        [sg.Button('Pagar', disabled=False if pago == 'Não' else True)]
+def cadastrar():
+    coluna_esquerda = [
+        [sg.Text('Placa:')],
+        [sg.Text('Carro:')],
+        [sg.Text('Cor:')],
+        [sg.Button('Cadastrar')]
     ]
-    
-    coluna_direita =[
-        [sg.Input(codigo, disabled=True, size=17, text_color='red', font=('Arial', '10', 'bold'), justification='right')],
-        [sg.Input(entrada, disabled=True, size=17)],
-        [sg.Input(saida, disabled=True, key='txtSaida', size=17)],
-        [sg.Input(tempo, disabled=True, size=17, justification='right')],
-        [sg.Input(pagar, disabled=True, key='txtPagar',size=17, font=('Arial', '10', 'bold'),justification='right')],
+
+    coluna_direita = [
+        [sg.Input(key='txtPlaca', size=20)],
+        [sg.Input(key='txtCarro', size=20)],
+        [sg.Input(key='txtCor', size=20)],
         [sg.Button('Sair')]
-    ]    
-    
-    layout = [
-        [sg.Column(coluna_esqueda), sg.Column(coluna_direita, element_justification='rigth')]        
     ]
 
-    window = sg.Window('Pagamento', layout, modal=True)
+    layout = [
+        [sg.Column(coluna_esquerda), sg.Column(coluna_direita, element_justification='right')]    
+    ]
+
+    window = sg.Window('Ticket', layout, modal=True)
 
     while True:
         events, values = window.read()
-        if events in (sg.WIN_CLOSED, 'Sair'):
+        if events in(sg.WIN_CLOSED, 'Sair'):
             break
-        
-        if events == 'Pagar':
-            try:
-                tb.pagar(ticket[0], ticket[2] if ticket[2] != '' else agora, tempo, valor)
-                sg.popup('Ticket pago')
-            except Exception as ex:
-                sg.popup_error(ex)          
-        
     window.close()
